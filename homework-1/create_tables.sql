942ca7e03545fe55047c17c63446726e6fc11193
AnnaToimurzina <2402as@gmail,com>
2023-07-05 23:33:00 +0500

    исправлена функция order

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -5,17 +5,29 @@ customer_id varchar(100) PRIMARY KEY,
 company_name varchar(100) NOT NULL,
 contact_name varchar(100) NOT NULL);
 
+
+
 CREATE TABLE employees(
 employee_id int PRIMARY KEY,
 first_name varchar(100) NOT NULL,
 last_name varchar(100) NOT NULL,
-title varchar(100) NOT NULL,
-birth_date date NOT NULL,
-notes varchar(100) NOT NULL);
+title varchar(500) NOT NULL,
+birth_date DATE NOT NULL,
+notes varchar(500) NOT NULL);
+
+
 
 CREATE TABLE orders(
 	order_id int PRIMARY KEY,
-	customer_id varchar(100) REFERENCES customers (customer_id) NOT NULL,
-	employee_id int REFERENCES employees (employee_id),
-	order_date date NOT NULL,
-	ship_city varchar(100) NOT NULL)
+	customer_id varchar(100) REFERENCES customers(customer_id)  NOT NULL,
+	employee_id int REFERENCES employees(employee_id),
+	order_date DATE NOT NULL,
+	ship_city varchar(100) NOT NULL);
+
+SELECT * FROM customers;
+SELECT * FROM employees;
+SELECT * FROM orders;
+
+DROP TABLE orders
+DROP TABLE customers;
+DROP TABLE employees;
--- homework-1/main.py
+++ homework-1/main.py
@@ -2,6 +2,7 @@ import psycopg2
 import csv
 
 
+
 def main():
         """Скрипт для заполнения данными таблиц в БД Postgres."""
 
@@ -15,23 +16,26 @@ cur = conn.cursor()
 try:
     with open('north_data\customers_data.csv', 'r') as file:
         reader = csv.reader(file)
+        next(reader)
         ''' Прочитать и вставить данные в таблицу'''
         for row in reader:
-            customer_id, company_name, contact_name = row
-            cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (customer_id, company_name, contact_name))
+            cur.execute('INSERT INTO customers ("customer_id", "company_name", "contact_name") VALUES (%s, %s, %s)', row)
+        conn.commit()
 
     with open('north_data\employees_data.csv', 'r') as file:
         reader = csv.reader(file)
+        next(reader)
         'Прочитать и вставить данные в таблицу'
         for row1 in reader:
-            employee_id, first_name, last_name, title, birth_date, notes = row1
-            cur.execute('INSERT INTO customers VALUES (%s, %s, %s, %s, %s, %s)', (employee_id, first_name, last_name, title, birth_date, notes))
+            cur.execute('INSERT INTO employees (employee_id, "first_name", "last_name", "title", "birth_date", "notes") VALUES (%s, %s, %s, %s, %s, %s)', row1)
+        conn.commit()
 
     with open('north_data\orders_data.csv', 'r') as file:
         reader = csv.reader(file)
+        next(reader)
         for row2 in reader:
-            order_id, customer_id, employee_id, order_date, ship_city = row2
-            cur.execute('INSERT INTO customers VALUES (%s, %s, %s, %s, %s, %s)', (order_id, customer_id, employee_id, order_date, ship_city))
+            cur.execute('INSERT INTO orders (order_id, "customer_id", employee_id, "order_date", "ship_city") VALUES (%s, %s, %s, %s, %s)', row2)
+        conn.commit()
 
 finally:
     conn.close()
